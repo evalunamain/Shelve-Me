723b70649724c165a90bd23375813ded011405c2
Eva Main <evalunamain@gmail.com>
2015-01-07 10:17:12 -0500

    Adds user table and model with authentication

--- shelf_life/app/models/user.rb
+++ shelf_life/app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
+  validates :password_digest, :session_token, :name, :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
 
   after_initialize :ensure_session_token
 
-  def self.find_by_credentials
-
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.valid_password?(password)
+    user
   end
 
   def password=(password)
@@ -15,6 +19,12 @@ class User < ActiveRecord::Base
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
   end
 
   private
